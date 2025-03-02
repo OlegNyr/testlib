e47476e4527cf66a69efe20a06cee2f942e1c0c2
onyrk <o.nyrkov@gmail.com>
2025-03-02 16:20:09 +0300

    DD

---
+++ src/main/java/ru/nyrk/libraly/MyClassMain.java
@@ -0,0 +1,10 @@
+package ru.nyrk.libraly;
+
+import org.apache.commons.lang.ArrayUtils;
+
+public class MyClassMain {
+    public static void main(String[] args) {
+        System.out.println(ArrayUtils.toString(args));
+
+    }
+}
